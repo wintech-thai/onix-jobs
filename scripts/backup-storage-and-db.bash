b10e790c9e62b2286085888eb08d4149b60744eb
pjames <pjames@gmail.com>
2024-02-29 21:05:09 +0700

    Added pgdump code

--- scripts/backup-storage-and-db.bash
+++ scripts/backup-storage-and-db.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 pwd
-ls -al
+
+DST_DIR=/tmp
+TS=$(date +%Y%m%d_%H%M%S)
+DMP_FILE=onix-acdsign-backup-${TS}.sql
+
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+
+pg_dump --dbname="postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:5432/${PG_DATABASE}" > ${DST_DIR}/${DMP_FILE}
+
+ls -al ${DST_DIR}
