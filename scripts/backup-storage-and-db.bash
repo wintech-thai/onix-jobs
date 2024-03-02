746268845f23af5450905f74a8ec97ffff760a63
pjames <pjames@gmail.com>
2024-03-02 07:08:01 +0700

    Fixed get pod name logic

--- scripts/backup-storage-and-db.bash
+++ scripts/backup-storage-and-db.bash
@@ -14,7 +14,7 @@ POD_SRC_DIR=/wis/data/storage
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 pg_dump --dbname="postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:5432/${PG_DATABASE}" > ${DST_DIR}/${DMP_FILE}
 
-POD_NAME=$(kubectl get pods -n ${NS} | grep ${NAME_PREFIX} | head -1)
+POD_NAME=$(kubectl get pods -n ${NS} | grep ${NAME_PREFIX} | head -1 | cut -f1 -d' ')
 
 echo "POD_NAME = [${POD_NAME}]"
 kubectl cp ${NS}/${POD_NAME}:${POD_SRC_DIR} ${DST_DIR}/storage
